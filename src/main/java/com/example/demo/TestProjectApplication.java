55b9ef057e2bd4cdd0e29ada9384be1516d19ddd
姚凯 <yaokai@gotokeep.com>
2018-11-03 13:56:32 +0800

    第一次修改

--- src/main/java/com/example/demo/TestProjectApplication.java
+++ src/main/java/com/example/demo/TestProjectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestProjectApplication {
-
+    //第一次修改
 	public static void main(String[] args) {
 		SpringApplication.run(TestProjectApplication.class, args);
 	}
